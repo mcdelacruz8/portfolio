27e7fa7c3fce6306469f4ec2dff0833c9ad990a2
mcdelacruz8 <88753390+mcdelacruz8@users.noreply.github.com>
2023-01-11 18:06:13 -0800

    Add files via upload

---
+++ components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-[#e26c1e]'>About</p>
+          </div>
+          <div>
+          </div>
+        </div>
+        <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+          <div className='sm:text-right text-4xl font-bold'>
+            <p>Hi. I'm Matthew, nice to meet you. Please take a look around.</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, enim magnam tempora non facilis culpa sit rerum in provident, at ut totam asperiores quisquam atque possimus, quia porro doloremque explicabo.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About;
